23b76bb082b785e7bdc90a3aa7c9d6e2c67ced78
ColdRollJoe <joseph.jr2522@gmail.com>
2018-08-04 08:55:41 -0400

    The main is a small program of running out of ammo

--- src/JoeBags/StoryTime.java
+++ src/JoeBags/StoryTime.java
@@ -24,29 +24,37 @@ public class StoryTime {
 //		Weapon a = new Gun("Sig 226", 1, 45);
 //		System.out.println(a);
 	
-		Gun b = new Gun();
-		b.setName("Sniper");
-		b.setDmg(10);
-		b.setRange(600);
-		b.setMagCap(5);  
-		System.out.println(b);
+		Gun sniper = new Gun();
+		sniper.setName("Barret .50 cal");
+		sniper.setDmg(10);
+		sniper.setRange(600);
+		sniper.setMagCap(5);  
+		System.out.println(sniper);
 		
+		Gun c = new Gun();
+		c.setMagCap(18);
 
 		
 		
 		 
 		do {
-			int r = b.getMagCap();
-			if (r >= 0) {
-				r -= 1;
-				b.setMagCap(r);
-				System.out.println("Would you like to take another shot? y/n");
+			int r = sniper.getMagCap();
+			if (r > 0) {
+				System.out.println("Would you like to take a shot? y/n");
 				shoot = scan.nextLine();
-				System.out.println(b.getMagCap());
+				r -= 1;
+				sniper.setMagCap(r);
+				System.out.println("There are " + sniper.getMagCap() + " rounds left in your magazine");
 			} 
+			else if (sniper.getMagCap() == 0) { 
+				System.out.println("Your " + sniper.getName() + " needs to be relaoded!");
+				break;
+			}
 		} while (shoot.equalsIgnoreCase("y"));
 		
-		System.out.println("You have " + b.getMagCap() + " rounds left.");
+		
+				
+				
 		
 		
 		
